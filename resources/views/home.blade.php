97cf391a4f721cbac0a7e0b755f803bb38093af9
SantiCHP <santichadmin@gmail.com>
2024-03-28 22:09:39 +0000

    Let's begin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <H1>Bienvenido a la web de Laravel 10 de SantiCHP</H1>
+</body>
+</html>
